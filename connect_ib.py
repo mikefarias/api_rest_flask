dc2a5ece9cf5dab3c726884c4463a77806136096
mike <mike@shoppingonlinearacaju.com.br>
2020-03-11 00:44:45 -0300

    criando rotas da api

---
+++ connect_ib.py
@@ -0,0 +1,30 @@
+from flask import Flask, jsonify, request
+import json
+
+connect_ib = Flask(__name__)
+
+#objetivo é pingar na API IB de 5 em 5 min para verificar status da bolsa
+#deve ser passado intervalo de tempo ?
+class Status:
+    @connect_ib.route("/ib/status", methods=['GET'])
+    def get_status_ib():
+        response = {'status':'teste'}
+        return jsonify(response)       
+
+
+class Trade:
+    @connect_ib.route("/ib/trade", methods=['GET'])
+    def get_trade_ib():
+        response = {'trade':'teste'}
+        return jsonify(response)       
+
+
+class Price:
+    @connect_ib.route("/ib/price", methods=['GET'])
+    def get_price_ib():
+        response = {'price':'teste'}
+        return jsonify(response)
+
+
+if __name__ == '__main__':
+    connect_ib.run(debug=True)
