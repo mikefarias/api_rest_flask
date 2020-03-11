2ebeb2f5eb8e1e3e01bb09ecabbe67f7035ebe87
mike <mike@shoppingonlinearacaju.com.br>
2020-03-11 00:50:47 -0300

    Ajuste das rotas para POST

--- connect_ib.py
+++ connect_ib.py
@@ -6,21 +6,21 @@ connect_ib = Flask(__name__)
 #objetivo é pingar na API IB de 5 em 5 min para verificar status da bolsa
 #deve ser passado intervalo de tempo ?
 class Status:
-    @connect_ib.route("/ib/status", methods=['GET'])
+    @connect_ib.route("/ib/status", methods=['POST'])
     def get_status_ib():
         response = {'status':'teste'}
         return jsonify(response)       
 
 
 class Trade:
-    @connect_ib.route("/ib/trade", methods=['GET'])
+    @connect_ib.route("/ib/trade", methods=['POST'])
     def get_trade_ib():
         response = {'trade':'teste'}
         return jsonify(response)       
 
 
 class Price:
-    @connect_ib.route("/ib/price", methods=['GET'])
+    @connect_ib.route("/ib/price", methods=['POST'])
     def get_price_ib():
         response = {'price':'teste'}
         return jsonify(response)
